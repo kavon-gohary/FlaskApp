c116ce87d3f159bd9f5712fe680ae40c8e2d77db
osasa <you@example.com>
2017-07-05 21:17:02 -0500

    first push to repo for flask app. has basic static features.

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import render_template           #modularity of flask. even though temrplates are neccesary for anythong
+                                            #flask will not force it on you.  really good quality
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+# @app.route("/")
+# @app.route("/<name>")
+# def hello(name=None):
+#     return render_template("hello.html", name=name)
+
+@app.route("/hello")
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route("/")
+@app.route("/home")
+def main_pg():
+    return render_template('frontpg.html')
+
+@app.route('/about')
+def person():
+    return render_template('user.html')
+
+@app.route('/contact')
+def contacts():
+    return render_template('contact2.html')
+#
+# @app.route("/double/<int:num>")
+# def show_num(num):
+#     return "%d" % (num+3)
+#     #return num         for some reason this throws an error
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/frontpg.js
@@ -0,0 +1,14 @@
+alert("are you 18 years or older and willing to see adult content?")
+
+
+list-style-type: none;      /*cancel bullet points*/
+   display: inline-block;      /*make the list side to side NOT up and down*/
+   margin: 1%;
+   /*padding: 20px 40px;*/           /*top/bottom and left/right*/
+   padding: 10px;
+   background: #999;
+   border-radius: 10px;
+   width: 18%;
+   float: left;
+   text-align: center;
+   color: white;
